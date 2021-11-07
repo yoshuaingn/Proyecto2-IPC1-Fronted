fdf9b59bdf393919d1e33a95f7e6639612092003
yoshua2712 <yoshuatecun7@gmail.com>
2021-11-07 12:46:51 -0600

    otra vez

--- index/funciones.js
+++ index/funciones.js
@@ -2,7 +2,7 @@
 let headers = new Headers()
 headers.append('Content-Type', 'application/json');
 headers.append('Accept', 'application/json');
-headers.append('Access-Control-Allow-Origin', 'https://proyecto2-ipc1-fronted.herokuapp.com');
+headers.append('Access-Control-Allow-Origin', 'https://proyecto2-ipc1-fronted.herokuapp.com/');
 headers.append('Access-Control-Allow-Credentials', 'true');
 headers.append('GET', 'POST', 'OPTIONS') ;
 
